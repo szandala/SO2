c54deb03ce9a9015d42fa37d95dead272fb959f4
Tomasz Szandala <Tomasz.Szandala@gmail.com>
2021-03-10 21:07:23 +0100

    Lab 1

---
+++ kanai_cube.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+if [[ -z "${1:-}" ]]; then
+    echo "Podaj nazwe katalogu, jaki mam utworzyc"
+    exit 1
+fi
+
+mkdir "${1}"
+cd "${1}"
+
+RECIPES=(skill_of_nilfur hope_of_cain
+    work_of_cathan caldesann_despair natala_prayer
+    law_of_kulle blessing_of_caban darkness_of_radament
+    archive_of_tal_rasha not_the_cow_level)
+
+for RECIPE in ${RECIPES[@]}; do
+    [[ $((RANDOM % 2)) -eq 0 ]] && { mkdir "${RECIPE}"; touch "${RECIPE}/some_content"; } || touch "${RECIPE}"
+    [[ $((RANDOM % 10)) -lt 4 ]] || echo "${RECIPE}" >> 2remove
+done
