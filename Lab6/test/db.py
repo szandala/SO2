47936d228f168f6133e7964d42b987c32edc03ad
Tomasz Szandala <Tomasz.Szandala@gmail.com>
2022-03-13 21:25:17 +0100

    Clean

--- Lab6/test/db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-
-def add_user(username):
-    user = {"username": username}
-    client = MongoClient("database", 27017)
-
-    users = client.my_db.users
-    user_id = users.insert_one(user)
