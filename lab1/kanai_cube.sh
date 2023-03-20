2a87288cf26e00b2325dec7328e3bea14a559153
Tomasz Szandala <Tomasz.Szandala@gmail.com>
2023-03-20 17:03:22 +0100

    Update of the cube

--- lab1/kanai_cube.sh
+++ lab1/kanai_cube.sh
@@ -9,9 +9,9 @@ mkdir "${1}"
 cd "${1}"
 
 RECIPES=(skill_of_nilfur hope_of_datco
-    work_of_cathan seraphin_despair shandala_prayer
+    work_of_angered seraphin_despair shandala_prayer
     law_of_kulle blessing_of_caban darkness_of_radament
-    archive_of_tal_rasha not_the_cow_level)
+    archive_of_iron_name not_the_cow_level)
 
 for RECIPE in ${RECIPES[@]}; do
     [[ $((RANDOM % 2)) -eq 0 ]] && { mkdir "${RECIPE}"; touch "${RECIPE}/some_content"; } || touch "${RECIPE}"
